5ad9cb0e11c657eeec9aee3ef87549214a62889d
witlox <pim.witlox@gmail.com>
2015-06-04 10:53:15 +0200

    updated to ini file again due to getopt_long() error

--- controller/start.sh
+++ controller/start.sh
@@ -7,10 +7,8 @@ docker pull witlox/wjc
 # start the database backend (runs on port 27017)
 docker run --name dcs-mongo -d mongo
 # start nginx with the overwritten default site (port 80)
-docker run -p 80:80 --name dcs-nginx -v nginx.conf:/etc/nginx/sites-enabled/default:ro -d nginx
+docker run -p 80:80 --name dcs-nginx -v $(pwd)/nginx.conf:/etc/nginx/sites-enabled/default:ro -d nginx
 # start uwsgi containers with overwritten config and mounted apps (ports 6000 & 7000)
 docker run --name dcs-ilm -d witlox/ilm
 docker run --name dcs-wjc -d witlox/wjc
 # and now everything should be up and running
-
-
