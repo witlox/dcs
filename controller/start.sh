04491e608bedd73680ea3cd2915aeba7d124d073
witlox <pim.witlox@gmail.com>
2015-06-24 15:27:58 +0200

    updated configs to use links

--- controller/start.sh
+++ controller/start.sh
@@ -5,10 +5,10 @@ docker pull mongo
 docker pull witlox/ilm
 docker pull witlox/wjc
 # start the database backend (runs on port 27017)
-docker run --name mongo -d mongo
-# start nginx with the overwritten default site (port 80)
-docker run -p 80:80 --name nginx -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
+docker run --name db -d mongo
 # start uwsgi containers with overwritten config and mounted apps (ports 6000 & 7000)
-docker run --name dcs-ilm -d witlox/ilm
-docker run --name dcs-wjc -d witlox/wjc
+docker run --name ilm --link db:db -d witlox/ilm
+docker run --name wjc --link db:db -d witlox/wjc
+# start nginx with the overwritten default site (port 80)
+docker run -p 80:80 --name web --link ilm:ilm --link wjc:wjc -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
 # and now everything should be up and running
